3c60fb2c33a06b312aefd10da1b2b6d3be984c5d
stanwon <stan_wong@foxmail.com>
2023-07-23 15:48:22 +0800

    basic config

--- init.lua
+++ init.lua
@@ -11,4 +11,21 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+vim.g.mapleader = " "
+vim.o.termguicolors = true
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.cursorline = true
+vim.o.ignorecase = true
+vim.o.expandtab = true
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.o.autoindent = true
+vim.o.wrap = true
+vim.o.autochdir = true
+vim.o.list = true
+vim.o.listchars = "tab:> ,trail:▫"
+
 require("lazy").setup({})
+
