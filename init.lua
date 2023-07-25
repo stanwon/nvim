ef6c082f85f21b21f34f6b41501f482ee2c0a29d
stan <stan_wong@foxmail.com>
2023-07-25 14:41:27 +0800

    add telescope-command-palette

--- init.lua
+++ init.lua
@@ -325,7 +325,6 @@ local lualine = {
 local comment = {
   "numToStr/Comment.nvim",
   config = function()
-    local api = require("Comment.api")
     require("Comment").setup({
       padding = true,
       stickly = true,
@@ -349,6 +348,7 @@ local telescope = {
     "nvim-lua/plenary.nvim",
     "LukasPietzschmann/telescope-tabs",
     "FeiyouG/command_center.nvim",
+    "LinArcX/telescope-command-palette.nvim"
   },
   config = function()
     local m = { noremap = true, nowait = true }
@@ -369,11 +369,54 @@ local telescope = {
         prompt_prefix = "🔍 ",
         selection_caret = " ",
       },
+      --[[ extensions = {
+        command_palette = {
+          {"File",
+            { "entire selection (C-a)", ':call feedkeys("GVgg")' },
+            { "save current file (C-s)", ':w' },
+            { "save all files (C-A-s)", ':wa' },
+            { "quit (C-q)", ':qa' },
+            { "file browser (C-i)", ":lua require'telescope'.extensions.file_browser.file_browser()", 1 },
+            { "search word (A-w)", ":lua require('telescope.builtin').live_grep()", 1 },
+            { "git files (A-f)", ":lua require('telescope.builtin').git_files()", 1 },
+            { "files (C-f)",     ":lua require('telescope.builtin').find_files()", 1 },
+          },
+          {"Help",
+            { "tips", ":help tips" },
+            { "cheatsheet", ":help index" },
+            { "tutorial", ":help tutor" },
+            { "summary", ":help summary" },
+            { "quick reference", ":help quickref" },
+            { "search help(F1)", ":lua require('telescope.builtin').help_tags()", 1 },
+          },
+          {"Vim",
+            { "reload vimrc", ":source $MYVIMRC" },
+            { 'check health', ":checkhealth" },
+            { "jumps (Alt-j)", ":lua require('telescope.builtin').jumplist()" },
+            { "commands", ":lua require('telescope.builtin').commands()" },
+            { "command history", ":lua require('telescope.builtin').command_history()" },
+            { "registers (A-e)", ":lua require('telescope.builtin').registers()" },
+            { "colorshceme", ":lua require('telescope.builtin').colorscheme()", 1 },
+            { "vim options", ":lua require('telescope.builtin').vim_options()" },
+            { "keymaps", ":lua require('telescope.builtin').keymaps()" },
+            { "buffers", ":Telescope buffers" },
+            { "search history (C-h)", ":lua require('telescope.builtin').search_history()" },
+            { "paste mode", ':set paste!' },
+            { 'cursor line', ':set cursorline!' },
+            { 'cursor column', ':set cursorcolumn!' },
+            { "spell checker", ':set spell!' },
+            { "relative number", ':set relativenumber!' },
+            { "search highlighting (F12)", ':set hlsearch!' },
+          }
+        }
+      } ]]
     })
     vim.keymap.set("n", "<leader>ff", builtin.find_files, m)
+    vim.keymap.set("n", "<leader>;", builtin.commands, m)
     vim.keymap.set("n", "<leader>fb", builtin.buffers, m)
     ts.load_extension("telescope-tabs")
     ts.load_extension("command_center")
+    -- ts.load_extension("command_palette")
   end,
 }
 
