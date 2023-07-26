b0aed0a9401f1aab1fab48629ae8724d5d0d9aa0
stan <stan_wong@foxmail.com>
2023-07-26 18:44:46 +0800

    todo add inlay_hint

--- init.lua
+++ init.lua
@@ -693,6 +693,20 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
   end
 })
 
+vim.api.nvim_create_augroup("LspAttach_inlayhints", {})
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = "LspAttach_inlayhints",
+  callback = function(args)
+    if not (args.data and args.data.client_id) then
+      return
+    end
+
+    local bufnr = args.buf
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    require("lsp-inlayhints").on_attach(client, bufnr)
+  end,
+})
+
 vim.keymap.set("n", "S", ":w<cr>", m)
 vim.keymap.set("n", "Q", ":q<cr>", m)
 vim.keymap.set("n", "sk", ":set nosplitbelow<CR>:split<CR>:set splitbelow<CR>")
