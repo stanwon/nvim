3722a684a3576dfb8c5755f83d89eebf0962af69
stanwon <stan_wong@foxmail.com>
2025-12-14 16:39:48 +0800

    refactor

--- init.lua
+++ init.lua
@@ -1,15 +1,8 @@
 require('stan.load_lazy')
 require('stan.options')
 require('stan.keymaps')
-require('stan.ls.clangd')
-require('stan.ls.lua_ls')
+require('stan.lsp.clangd')
+require('stan.lsp.lua_ls')
+require('stan.personal')
 
 require('stan.aerial').setup()
-vim.cmd [[colorscheme unokai]]
-
-vim.api.nvim_create_autocmd('VimEnter', {
-    pattern = "*",
-    callback = function()
-        vim.cmd [[Yazi]]
-    end
-})
---
+++ lua/stan/personal.lua
@@ -0,0 +1,10 @@
+vim.cmd [[colorscheme unokai]]
+
+vim.api.nvim_create_autocmd('VimEnter', {
+    pattern = "*",
+    callback = function()
+        if vim.fn.argc() == 0 then
+            vim.cmd [[Yazi]]
+        end
+    end
+})
