0c2f6d72b67aa5d98f1718f4eb4ff004c3775314
stan <806552980@qq.com>
2025-12-18 10:25:07 +0800

    change window size of zen mode

--- lua/stan/zen.lua
+++ lua/stan/zen.lua
@@ -5,7 +5,7 @@ M.setup = function()
         require("zen-mode").toggle({
             window = {
                 backdrop = 0.95, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
-                width = .6,      -- width of the Zen window
+                width = .75,      -- width of the Zen window
                 height = 1,      -- height of the Zen window
                 options = {
                     -- signcolumn = "no", -- disable signcolumn
