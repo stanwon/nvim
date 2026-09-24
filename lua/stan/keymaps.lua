e7615801b5a2136e01581095d98c2a7c7891c81e
stan <806552980@qq.com>
2026-09-24 11:23:18 +0800

    change: restore LSP gd mapping with vscode guard

--- lua/stan/keymaps.lua
+++ lua/stan/keymaps.lua
@@ -16,7 +16,11 @@ vim.keymap.set('n', 'tl', ':tabnext<cr>', { noremap = true, desc = '下一个标
 
 -- 代码
 vim.keymap.set('n', 'gr', ':lua vim.lsp.buf.references()<cr>', { noremap = true, desc = 'LSP 查找引用' })
--- vim.keymap.set('n', 'gd', ':lua vim.lsp.buf.definition()<cr>', { noremap = true, desc = 'LSP 跳转定义' })
+if vim.g.vscode then
+  -- 交给 vscode-neovim 默认的 gd（revealDefinition），不要覆盖
+else
+  vim.keymap.set('n', 'gd', ':lua vim.lsp.buf.definition()<cr>', { desc = 'LSP 跳转定义' })
+end
 vim.keymap.set('n', 'gD', ':lua vim.lsp.buf.declaration()<cr>', { noremap = true, desc = 'LSP 跳转声明' })
 vim.keymap.set('n', '<leader>fm', ':lua vim.lsp.buf.format()<cr>', { noremap = true, desc = '格式化代码' })
 
